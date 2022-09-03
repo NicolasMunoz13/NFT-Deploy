f70398b5946fc430af564959340331ee175510a6
NicolasMunoz13 <juanicolasmuozcampos@gmail.com>
2022-09-03 02:55:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x951271A2772569c7A1f39ba38718DdAd1AAEd4E4";
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
